78fdaaacbc967df46678f9a4bc1fb6a3f5eb301a
amirhamzan <amirhamzan30@gmail.com>
2026-02-13 11:53:54 +0800

    Create columns for employees table structure

--- database/migrations/2026_02_13_114530_create_employees_table.php
+++ database/migrations/2026_02_13_114530_create_employees_table.php
@@ -13,6 +13,12 @@ return new class extends Migration
     {
         Schema::create('employees', function (Blueprint $table) {
             $table->id();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->unsignedBigInteger('company_id');
+            $table->foreign('company_id')->references('id')->on('companies');
+            $table->string('email')->nullable();
+            $table->string('phone')->nullable();
             $table->timestamps();
         });
     }
